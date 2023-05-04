d7b9623dde9a5e49f1bff4179dc0ccabd084f12b
Simon <wei14@seas.upenn.edu>
2023-05-04 17:11:44 -0400

    create deliverBooksFromOneWareHouse and deliverBooksFromMultiWareHouse

---
+++ src/src/mehtod.java
@@ -0,0 +1,16 @@
+package src;
+
+import src.DataProcessor.DataProcessor;
+
+public class mehtod implements methods{
+    @Override
+    public int deliverBooksFromOneWareHouse() {
+        DataProcessor dp =  new DataProcessor();
+        return 0;
+    }
+
+    @Override
+    public int deliverBooksFromMultiWareHouse() {
+        return 0;
+    }
+}
