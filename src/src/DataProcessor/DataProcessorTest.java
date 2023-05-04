fa4d33840c8c7386a273a1358327ad98b522ce9d
Simon <wei14@seas.upenn.edu>
2023-05-04 16:24:13 -0400

    add the DataProcessorTest

---
+++ src/src/DataProcessor/DataProcessorTest.java
@@ -0,0 +1,21 @@
+package src.DataProcessor;
+
+import org.junit.jupiter.api.Test;
+
+class DataProcessorTest {
+
+    @Test
+    void parseCities() {
+
+    }
+
+    @Test
+    void parseBooks() {
+
+    }
+
+    @Test
+    void initialization() {
+
+    }
+}
